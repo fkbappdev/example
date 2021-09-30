0cec8fb653027738ad25d6359fd068d4cb18320f
fkbappdev <90206573+fkbappdev@users.noreply.github.com>
2021-09-29 22:47:55 -0500

    Create styles.js

---
+++ src/components/Profile/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  image: {
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "white",
+  }
+});
+
+export default styles;
